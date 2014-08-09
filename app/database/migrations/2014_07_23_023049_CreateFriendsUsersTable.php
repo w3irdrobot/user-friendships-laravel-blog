b48ad9ea7bd26f6bd6c11681be40e5fc028e52d3
Alex Sears <alexwsears@gmail.com>
2014-08-09 18:56:04 -0400

    Added foreign keys and composite key to composite table

--- app/database/migrations/2014_07_23_023049_CreateFriendsUsersTable.php
+++ app/database/migrations/2014_07_23_023049_CreateFriendsUsersTable.php
@@ -16,6 +16,11 @@ class CreateFriendsUsersTable extends Migration {
 		{
 			$table->integer('friend_id')->unsigned();
 			$table->integer('user_id')->unsigned();
+
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->foreign('friend_id')->references('id')->on('users');
+
+			$table->primary(array('user_id', 'friend_id'));
 		});
 	}
 
